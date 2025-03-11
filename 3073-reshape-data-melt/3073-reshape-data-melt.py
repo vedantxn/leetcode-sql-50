408e73886a766b5598e145ecf6d4ce31773862b4
Zuckerburger <vn.2006@outlook.com>
2025-03-11 14:17:07 +0530

    Time: 514 ms (50.35%), Space: 65.9 MB (79.62%) - LeetHub

---
+++ 3073-reshape-data-melt/3073-reshape-data-melt.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+	result = pd.melt(report , id_vars = ['product'] , var_name = 'quarter' , value_name = 'sales')
+	return result
+	
